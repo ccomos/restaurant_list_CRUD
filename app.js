df379617a27485ae7a31b85e66bc8a03475c35ec
wei <fhaowei@yahoo.com.tw>
2020-05-20 01:09:58 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
 //require packages used in project
 const express = require('express')
-const app = express()
 const port = 3000
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-
-//require express-handlebars here
 const exphbs = require('express-handlebars')
-const restaurant = require('./models/restaurant') //填寫路徑載入檔案
+const restaurant = require('./models/restaurant')
 const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-//connect with mongodb
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(routes)
-
-// setting static files
-app.use(express.static('public'))
+app.use(express.static('public')) // setting static files
+app.use(routes) //express router is default to find index.js
 
 //start and listen on the Express server
 app.listen(port, () => {
