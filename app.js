42d794bebc6c2ab644323316c5bf25344a283710
wei <fhaowei@yahoo.com.tw>
2020-07-13 23:03:07 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //require packages used in project
 const express = require('express')
+const session = require('express-session')
 const port = 3000
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -12,6 +13,12 @@ const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
